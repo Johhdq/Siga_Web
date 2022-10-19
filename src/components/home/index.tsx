884223d47a492ae93ebdb9a6aabe5ca888866ef6
João Augusto <johhdq@hotmail.com>
2022-10-18 23:04:41 -0300

    Ajustes telas

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,41 +1,42 @@
 
-import styles from './styles.module.css';
+import './styles.css';
+import logo from '../../assets/logo_siga.png';
 
 export function Home() {
     return (
-        <div className={styles.container}>
-            <div className={styles.leftBar}>
-                <div className={styles.perfil}>
-                    <div className={styles.imgContainer}>
-                        <div className={styles.img}>
+        <div className="container">
+            <div className="leftBar">
+                <div className="perfil">
+                    <div className="imgContainer">
+                        <div className="homeImg">
                             <h1>LO</h1>
                         </div>
                     </div>
 
-                    <div className={styles.informations}>
+                    <div className="informations">
                         <p><strong>Nome: </strong>Leba Omrac</p>
                         <p><strong>Email: </strong>leba.omrac@fatec.sp.gov.br</p>
                         <p><strong>RA: </strong>1190482013012</p>
                     </div>
                 </div>
 
-                <div className={styles.pageList}>
-                    <div className={styles.pageListItem}>
+                <div className="pageList">
+                    <div className="pageListItem">
                         <span>Item</span><i>-</i>
                     </div>
-                    <div className={styles.pageListItem}>
+                    <div className="pageListItem">
                         <span>Item</span><i>-</i>
                     </div>
-                    <div className={styles.pageListItem}>
+                    <div className="pageListItem">
                         <span>Item</span><i>-</i>
                     </div>
-                    <div className={styles.logo}>
-                        <h2>SIGA - Fatec</h2>
+                    <div className="logo">
+                        <img src={logo} alt="logoSiga" style={{width: "inherit"}} />
                     </div>
                 </div>
             </div>
-            <div className={styles.content}>
-                <h1>Bem-vindo ao SIGA - Fatec Garça</h1>
+            <div className="content">
+                <h1>Bem-vindo ao <strong>SIGA</strong> - Fatec Garça</h1>
                 <h2>Desenvolvido por: Abel Carmo, João Augusto, Kauan Medeiros, e Renan Medeiros</h2>
             </div>
         </div>
