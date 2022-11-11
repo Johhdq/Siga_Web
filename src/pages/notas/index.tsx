d3eb4572c0b07de37852c76a6d6349a6c07cea32
kutt.kauan <kauan_med@outlook.com>
2022-11-11 17:03:26 -0300

    Flex grid

--- src/pages/notas/index.tsx
+++ src/pages/notas/index.tsx
@@ -6,8 +6,9 @@ export function Notas() {
       <h1>Notas</h1>
       <div className="disciplinaContainer">
         <header className="disciplinaTopics">
-          <h2>Disciplina</h2>
-          <div style={{ display: "flex" }}>
+          <h2 className="disciplinaTopicsTitle">Disciplina</h2>
+
+          <div style={{ display: "flex", gap: 32 }}>
             <div className="notas">
               <h2>N1</h2>
               <h2>N2</h2>
@@ -19,23 +20,57 @@ export function Notas() {
 
         <div className="notasRow">
           <div className="disciplina">
-            <p>Gestão e Governança de Tecnologia da Informação</p>
+            <p className="disciplinaTitle">
+              Gestão e Governança de Tecnologia da Informação
+            </p>
           </div>
 
-          <div className="notasValues">
-            <div className="notaCard">
-              <p style={{color: "royalblue"}}>10</p>
+          <div style={{ display: "flex", gap: 32 }}>
+            <div className="notasValues">
+              <div className="notaCard">
+                <p style={{ color: "royalblue" }}>10</p>
+              </div>
+              <div className="notaCard">
+                <p style={{ color: "#b20000" }}>2</p>
+              </div>
+              <div className="notaCard">
+                <p>-</p>
+              </div>
             </div>
-            <div className="notaCard">
-              <p style={{color: "#b20000"}}>2</p>
-            </div>
-            <div className="notaCard">
-              <p>-</p>
+
+            <div className="mediaFinal">
+              <p style={{ color: "royalblue" }}>
+                <strong>6</strong>
+              </p>
             </div>
           </div>
+        </div>
 
-          <div className="mediaFinal">
-            <p style={{color: "royalblue"}}><strong>6</strong></p>
+        <div className="notasRow">
+          <div className="disciplina">
+            <p className="disciplinaTitle">
+              Tópicos Especiais em Informática (Escolha 2)
+            </p>
+          </div>
+
+          <div style={{ display: "flex", gap: 32 }}>
+            <div className="notasValues">
+              <div className="notaCard">
+                <p style={{ color: "royalblue" }}>10</p>
+              </div>
+              <div className="notaCard">
+                <p style={{ color: "#b20000" }}>2</p>
+              </div>
+              <div className="notaCard">
+                <p>-</p>
+              </div>
+            </div>
+
+            <div className="mediaFinal">
+              <p style={{ color: "royalblue" }}>
+                <strong>6</strong>
+              </p>
+            </div>
           </div>
         </div>
       </div>
