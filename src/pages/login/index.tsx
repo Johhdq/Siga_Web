337384e1371ab3dfbbf251791d684b58d1005d38
João Augusto Druzian <johhdq@hotmail.com>
2022-11-27 16:03:45 -0300

    adicionado requisições

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,44 +1,74 @@
 import "./styles.css";
-import logo from '../../assets/logo_siga.png';
+import logo from "../../assets/logo_siga.png";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth";
+import axios from "axios";
+
+type LoginResponse = {
+  sucess: string;
+  id: number;
+  typeUser: number;
+};
 
 export function Login() {
-    const navigate = useNavigate();
-    const { saveUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { saveUser } = useContext(AuthContext);
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log(event);
-        let cpf = (document.getElementById("login") as HTMLInputElement).value;
-        let senha = (document.getElementById("senha") as HTMLInputElement).value;
+  const baseUrl = "url";
 
-        console.log(`CPF: ${cpf}; SENHA: ${senha}`);
-        
-        const loginResponse = 1 //fazer chamada na api
-        saveUser(loginResponse)
-        
-        navigate('/');
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    let credentials = {
+      cpf: (document.getElementById("login") as HTMLInputElement).value,
+      senha: (document.getElementById("senha") as HTMLInputElement).value,
+    };
+
+    try {
+      const loginResponse: LoginResponse = await axios.post(`${baseUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+
+      if (!loginResponse) {
+        console.log("erro ao realizar login");
+        return;
+      }
+
+      saveUser(loginResponse.id);
+      navigate("/");
+
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div className="loginContent">
-            <div className="img">
-                <img src={logo} alt="Logo-siga" />
-            </div>
+  return (
+    <div className="loginContent">
+      <div className="img">
+        <img src={logo} alt="Logo-siga" />
+      </div>
 
-            <form className="loginForm" onSubmit={handleSubmit}>
-                <div className="input">
-                    <span>Login:</span>
-                    <input type="text" id="login" placeholder="Digite seu CPF" required />
-                </div>
-                <div className="input">
-                    <span>Senha:</span>
-                    <input type="password" id="senha" placeholder="Digite sua senha" required />
-                </div>
-                <button type="submit">Entrar</button>
-            </form>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <div className="input">
+          <span>Login:</span>
+          <input type="text" id="login" placeholder="Digite seu CPF" required />
         </div>
-    )
+        <div className="input">
+          <span>Senha:</span>
+          <input
+            type="password"
+            id="senha"
+            placeholder="Digite sua senha"
+            required
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/notas/index.tsx
+++ src/pages/notas/index.tsx
@@ -1,6 +1,39 @@
 import "./styles.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth";
+import { useState } from "react";
+import { MdNotStarted } from "react-icons/md";
+
+type Grade = {
+  n1: number | null;
+  n2: number | null;
+  n3: number | null;
+  subjectId: number;
+  subjectName: string;
+};
 
 export function Notas() {
+  const { user } = useContext(AuthContext);
+  const [grades, setGrade] = useState<Grade[]>([]);
+  const baseUrl = "url";
+
+  // Resgatando dados
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/notas/${user?.id}`)
+      .then((response) => {
+        if (!response) {
+          console.log("erro ao consultar");
+        }
+        setGrade(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
     <div className="contentNotas">
       <h1>Notas</h1>
@@ -18,13 +51,45 @@ export function Notas() {
           </div>
         </header>
 
+        {grades &&
+          grades.map((grade) => (
+            <div className="notasRow">
+              <div className="disciplina">
+                <p className="disciplinaTitle">{grade.subjectName}</p>
+              </div>
+              <div style={{ display: "flex", gap: 32 }}>
+                <div className="notasValues">
+                  <div className="notaCard">
+                    <p style={{ color: "royalblue" }}>
+                      {grade.n1 ? grade.n1 : "-"}
+                    </p>
+                  </div>
+                  <div className="notaCard">
+                    <p style={{ color: "#b20000" }}>
+                      {grade.n2 ? grade.n2 : "-"}
+                    </p>
+                  </div>
+                  <div className="notaCard">
+                    <p>{grade.n3 ? grade.n3 : "-"}</p>
+                  </div>
+                </div>
+
+                <div className="mediaFinal">
+                  <p style={{ color: "royalblue" }}>
+                    <strong>6</strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+
         <div className="notasRow">
           <div className="disciplina">
             <p className="disciplinaTitle">
               Gestão e Governança de Tecnologia da Informação
             </p>
           </div>
-
+          
           <div style={{ display: "flex", gap: 32 }}>
             <div className="notasValues">
               <div className="notaCard">
